e8fca7440bfa51232a23221255e72b67e4980d75
changyoung4087 <double4087@naver.com>
2023-04-14 12:03:05 +0900

    Initial commit

---
+++ src/test/java/org/koreait/test/BoardDAOTest.java
@@ -0,0 +1,34 @@
+package org.koreait.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.koreait.models.board.Board;
+import org.koreait.models.board.BoardDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest // @Transactional
+public class BoardDAOTest {
+    @Autowired
+    private BoardDAO boardDao;
+
+    @Test @DisplayName("게시글 추가 테스트")
+    public void insertTest() {
+        Board board = new Board();
+        board.setSubject("제목");
+        board.setContent("내용");
+
+        boolean result = boardDao.insert(board);
+        assertTrue(result);
+
+    }
+
+    @Test @DisplayName("게시글 조회 테스트")
+    public void getTest(){
+        Board board = boardDao.get(3L);
+        System.out.println(board);
+    }
+}
